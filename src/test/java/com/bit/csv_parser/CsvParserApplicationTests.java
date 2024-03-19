3e1c87bfa465633291eb428e413513b570a7bdd2
Mescalindatura <lolitwasfree@gmail.com>
2024-03-19 10:51:54 +0200

    initial

---
+++ src/test/java/com/bit/csv_parser/CsvParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bit.csv_parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
